3e14ff79b341ed1cdc9ff1cb8da21a956cfdf6ed
Vincent Tan <vincenttan83@live.com>
2021-06-27 18:18:51 +0800

    export enum for consumption

--- projects/dynamic-form/src/public-api.ts
+++ projects/dynamic-form/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of dynamic-form
  */
 
+export * from './lib/enums/field-config-input-type.enum';
+export * from './lib/enums/field-config-type.enum';
 export * from './lib/dynamic-field.directive';
 export * from './lib/dynamic-form.component';
 export * from './lib/dynamic-form.module';
