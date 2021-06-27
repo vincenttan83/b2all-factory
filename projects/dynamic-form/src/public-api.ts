e7b29f2217f7342512c4608673e693d40dd6bc8d
Vincent Tan <vincenttan83@live.com>
2021-06-27 16:48:23 +0800

    prep for npmjs package

--- projects/dynamic-form/src/public-api.ts
+++ projects/dynamic-form/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of dynamic-form
  */
 
-export * from './lib/dynamic-form.service';
+export * from './lib/dynamic-field.directive';
 export * from './lib/dynamic-form.component';
 export * from './lib/dynamic-form.module';
