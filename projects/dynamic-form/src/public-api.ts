84c467de43e1c60142933f3f46b9bc11f6114d28
Vincent Tan <vincenttan83@live.com>
2021-06-28 17:37:22 +0800

    export css class interface

--- projects/dynamic-form/src/public-api.ts
+++ projects/dynamic-form/src/public-api.ts
@@ -6,6 +6,7 @@ export * from './lib/enums/field-config-input-type.enum';
 export * from './lib/enums/field-config-type.enum';
 export * from './lib/interfaces/field-config.interface';
 export * from './lib/interfaces/multi-select.interface';
+export * from './lib/interfaces/css-class.interface';
 export * from './lib/dynamic-field.directive';
 export * from './lib/dynamic-form.component';
 export * from './lib/dynamic-form.module';
