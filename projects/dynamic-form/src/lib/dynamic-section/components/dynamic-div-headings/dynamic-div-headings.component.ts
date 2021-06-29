47c1f18504fea5a329d19cf04310e63c8201f50f
Vincent Tan <vincenttan83@live.com>
2021-06-29 22:07:05 +0800

    implemented IDiv

--- projects/dynamic-form/src/lib/dynamic-section/components/dynamic-div-headings/dynamic-div-headings.component.ts
+++ projects/dynamic-form/src/lib/dynamic-section/components/dynamic-div-headings/dynamic-div-headings.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { IDivConfig, IDivConfigForHeadings } from '../../interfaces/div-config.interface';
+import { IDiv } from '../../interfaces/div.interface';
 
 @Component({
   selector: 'b2all-dynamic-div-headings',
   templateUrl: './dynamic-div-headings.component.html',
   styleUrls: ['./dynamic-div-headings.component.css']
 })
-export class DynamicDivHeadingsComponent implements OnInit {
+export class DynamicDivHeadingsComponent implements OnInit, IDiv<IDivConfigForHeadings> {
+
+  config!: IDivConfig<IDivConfigForHeadings>;
+  index!: number;
 
   constructor() { }
 
