9eb924c620c55a257a282b1869a53e3b12d7c8c5
gorden <limguancheng@hotmail.com>
2021-11-17 12:33:02 +0800

    give multiselect proper type

--- projects/dynamic-form/src/lib/dynamic-form/components/multiselect-dataset/multiselect-dataset.component.ts
+++ projects/dynamic-form/src/lib/dynamic-form/components/multiselect-dataset/multiselect-dataset.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { getValidators } from '../../classes/custom-validator.class';
 import { EFieldConfigInputType } from '../../enums/field-config-input-type.enum';
 import { EFieldConfigType } from '../../enums/field-config-type.enum';
@@ -6,6 +14,9 @@ import { EFormValidator } from '../../enums/form-validator.enum';
 import { IFieldConfigForArrayConfig } from '../../interfaces/field-config-for-array.interface';
 import { IFieldConfigForButtonConfig } from '../../interfaces/field-config-for-button.interface';
 import { IFieldConfigForInputConfig } from '../../interfaces/field-config-for-input.interface';
+import { IFieldConfigForObjectConfig } from '../../interfaces/field-config-for-object.interface';
+import { IFieldConfigForSelectConfig } from '../../interfaces/field-config-for-select.interface';
+import { IFieldConfigForTextareaConfig } from '../../interfaces/field-config-for-textarea.interface';
 import { IFieldConfig } from '../../interfaces/field-config.interface';
 import { IMultiSelect } from '../../interfaces/multi-select.interface';
 import { DynamicFormComponent } from '../dynamic-form.component';
@@ -13,33 +24,40 @@ import { DynamicFormComponent } from '../dynamic-form.component';
 @Component({
   selector: 'b2all-multiselect-dataset',
   templateUrl: './multiselect-dataset.component.html',
-  styleUrls: ['./multiselect-dataset.component.css']
+  styleUrls: ['./multiselect-dataset.component.css'],
 })
 export class MultiselectDatasetComponent implements OnInit, AfterViewInit {
-
   @ViewChild(DynamicFormComponent) dynamicForm!: DynamicFormComponent;
 
   @Input() inputHierarchyLevels!: string[];
   @Input() inputData!: { [key: string]: any };
-  @Input() inputPreTemplate!: IFieldConfig<any>[];
-  @Input() inputPostTemplate!: IFieldConfig<any>[];
-  @Input() inputSubmitButtonTemplate!: IFieldConfig<IFieldConfigForButtonConfig>;
+  @Input() inputPreTemplate!: IFieldConfig<
+    | IFieldConfigForInputConfig
+    | IFieldConfigForSelectConfig
+    | IFieldConfigForTextareaConfig
+  >[];
+  @Input() inputPostTemplate!: IFieldConfig<
+    | IFieldConfigForInputConfig
+    | IFieldConfigForSelectConfig
+    | IFieldConfigForTextareaConfig
+  >[];
+  @Input()
+  inputSubmitButtonTemplate!: IFieldConfig<IFieldConfigForButtonConfig>;
   @Output() outputFormOnSubmit: EventEmitter<any> = new EventEmitter<any>();
   @Output() outputFormOnChange: EventEmitter<any> = new EventEmitter<any>();
 
   multiSelectTemplate: IFieldConfig<any>[] = [];
   formReady = false;
 
-  constructor() { }
+  constructor() {}
 
   ngAfterViewInit(): void {
-    this.dynamicForm.changes.subscribe(resp => {
+    this.dynamicForm.changes.subscribe((resp) => {
       this.outputFormOnChange.emit(resp);
     });
   }
 
   ngOnInit(): void {
-
     // generate the multiselect template based on the level received
     const theTemplate = this.generateTemplate(this.inputHierarchyLevels);
     // push the template into the fields stack
@@ -53,7 +71,6 @@ export class MultiselectDatasetComponent implements OnInit, AfterViewInit {
     this.multiSelectTemplate.push(this.inputSubmitButtonTemplate);
     // set the form state to ready so the dynamic form will start rendering
     this.formReady = true;
-
   }
 
   formOnSubmitting(val: any): void {
@@ -65,26 +82,45 @@ export class MultiselectDatasetComponent implements OnInit, AfterViewInit {
     this.outputFormOnSubmit.emit(newVal);
   }
 
-  generateTemplate(values: string[]): any {
+  generateTemplate(
+    values: string[]
+  ): IFieldConfig<IFieldConfigForArrayConfig<IFieldConfigForInputConfig>> {
     let lvlTemplate: any;
     for (let i = values.length - 1; i >= 0; i--) {
-      if (i === (values.length - 1)) {
-        lvlTemplate = this.generateTemplateForLastLevel(values[i], values.length - 1, i, ['Key', 'Value']);
+      if (i === values.length - 1) {
+        lvlTemplate = this.generateTemplateForLastLevel(
+          values[i],
+          values.length - 1,
+          i,
+          ['Key', 'Value']
+        );
       } else {
-        lvlTemplate = this.genearteTemplateForLevelHasChildren(values[i], lvlTemplate, values.length - 1, i, ['Key', 'Value', 'Children']);
+        lvlTemplate = this.generateTemplateForLevelHasChildren(
+          values[i],
+          lvlTemplate,
+          values.length - 1,
+          i,
+          ['Key', 'Value', 'Children']
+        );
       }
     }
     return lvlTemplate;
   }
 
-  generateTemplateForLastLevel(displayText: string, maxIndex: number, curIndex: number, columnNames: string[]): any {
+  generateTemplateForLastLevel(
+    displayText: string,
+    maxIndex: number,
+    curIndex: number,
+    columnNames: string[]
+  ): IFieldConfig<IFieldConfigForArrayConfig<IFieldConfigForInputConfig>> {
     return {
       name: 'children',
       display_text: displayText,
       type: EFieldConfigType.Array,
       type_config: {
         hierarchy_level: {
-          cur_level: curIndex, max_level: maxIndex,
+          cur_level: curIndex,
+          max_level: maxIndex,
         },
         table_column_names: columnNames,
         field_configs: [
@@ -95,9 +131,14 @@ export class MultiselectDatasetComponent implements OnInit, AfterViewInit {
             type_config: {
               type: EFieldConfigInputType.Text,
               list: false,
-              css_class: { group: '', group_label: '', input: 'form-control form-control-sm', input_label: '' }
+              css_class: {
+                group: '',
+                group_label: '',
+                input: 'form-control form-control-sm',
+                input_label: '',
+              },
             },
-            css_class: 'key-width-non-child'
+            css_class: 'key-width-non-child',
           },
           {
             name: 'value',
@@ -106,9 +147,14 @@ export class MultiselectDatasetComponent implements OnInit, AfterViewInit {
             type_config: {
               type: EFieldConfigInputType.Text,
               list: false,
-              css_class: { group: '', group_label: '', input: 'form-control form-control-sm', input_label: '' }
+              css_class: {
+                group: '',
+                group_label: '',
+                input: 'form-control form-control-sm',
+                input_label: '',
+              },
             },
-            css_class: 'val-width-non-child'
+            css_class: 'val-width-non-child',
           },
         ],
         css_class: {
@@ -116,23 +162,27 @@ export class MultiselectDatasetComponent implements OnInit, AfterViewInit {
           del_button: 'btn btn-sm btn-danger',
           group: '',
           group_label: '',
-          label: ''
-        }
+          label: '',
+        },
       },
     };
   }
 
-  genearteTemplateForLevelHasChildren(
-    displayText: string, theNextLevelTemplate: any,
-    maxIndex: number, curIndex: number, columnNames: string[]): any {
-
+  generateTemplateForLevelHasChildren(
+    displayText: string,
+    theNextLevelTemplate: IFieldConfig<IFieldConfigForInputConfig>,
+    maxIndex: number,
+    curIndex: number,
+    columnNames: string[]
+  ): IFieldConfig<IFieldConfigForArrayConfig<IFieldConfigForInputConfig>> {
     return {
       name: 'children',
       display_text: displayText,
       type: EFieldConfigType.Array,
       type_config: {
         hierarchy_level: {
-          cur_level: curIndex, max_level: maxIndex,
+          cur_level: curIndex,
+          max_level: maxIndex,
         },
         table_column_names: columnNames,
         hideRemoveRowItemButton: true,
@@ -144,9 +194,14 @@ export class MultiselectDatasetComponent implements OnInit, AfterViewInit {
             type_config: {
               type: EFieldConfigInputType.Text,
               list: false,
-              css_class: { group: '', group_label: '', input: 'form-control form-control-sm', input_label: '' }
+              css_class: {
+                group: '',
+                group_label: '',
+                input: 'form-control form-control-sm',
+                input_label: '',
+              },
             },
-            css_class: 'key-width-has-child'
+            css_class: 'key-width-has-child',
           },
           {
             name: 'value',
@@ -155,30 +210,39 @@ export class MultiselectDatasetComponent implements OnInit, AfterViewInit {
             type_config: {
               type: EFieldConfigInputType.Text,
               list: false,
-              css_class: { group: '', group_label: '', input: 'form-control form-control-sm', input_label: '' }
+              css_class: {
+                group: '',
+                group_label: '',
+                input: 'form-control form-control-sm',
+                input_label: '',
+              },
             },
-            css_class: 'val-width-has-child'
+            css_class: 'val-width-has-child',
           },
           {
-            ...theNextLevelTemplate
-          }
+            ...theNextLevelTemplate,
+          },
         ],
         css_class: {
           add_button: 'btn btn-sm btn-primary',
           del_button: 'btn btn-sm btn-danger',
           group: '',
           group_label: '',
-          label: ''
-        }
+          label: '',
+        },
       },
     };
   }
 
   recursiveSort(value: IMultiSelect): IMultiSelect {
     if (value && value.children) {
-      const sortedObject: IMultiSelect = { key: value.key, value: value.value, children: this.arrayOfObjectSort(value.children) };
+      const sortedObject: IMultiSelect = {
+        key: value.key,
+        value: value.value,
+        children: this.arrayOfObjectSort(value.children),
+      };
       const newChildren: IMultiSelect[] = [];
-      sortedObject.children?.forEach(element => {
+      sortedObject.children?.forEach((element) => {
         newChildren.push(this.recursiveSort(element));
       });
       return { key: value.key, value: value.value, children: newChildren };
@@ -198,5 +262,4 @@ export class MultiselectDatasetComponent implements OnInit, AfterViewInit {
       return 0;
     });
   }
-
 }
--- src/app/components/multi-select/multi-select.component.ts
+++ src/app/components/multi-select/multi-select.component.ts
@@ -1,60 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 import { EFieldConfigType } from 'projects/dynamic-form/src/lib/dynamic-form/enums/field-config-type.enum';
 import { IFieldConfigForButtonConfig } from 'projects/dynamic-form/src/lib/dynamic-form/interfaces/field-config-for-button.interface';
+import { IFieldConfigForInputConfig } from 'projects/dynamic-form/src/lib/dynamic-form/interfaces/field-config-for-input.interface';
+import { IFieldConfigForSelectConfig } from 'projects/dynamic-form/src/lib/dynamic-form/interfaces/field-config-for-select.interface';
 import { IFieldConfig } from 'projects/dynamic-form/src/lib/dynamic-form/interfaces/field-config.interface';
+import {
+  EFieldConfigInputType,
+  IMultiSelect,
+} from 'projects/dynamic-form/src/public-api';
 
 @Component({
   selector: 'app-multi-select',
   templateUrl: './multi-select.component.html',
-  styleUrls: ['./multi-select.component.sass']
+  styleUrls: ['./multi-select.component.sass'],
 })
 export class MultiSelectComponent implements OnInit {
-
   multiselectHierarchyLevels: string[] = ['Country', 'State'];
-  multiselectData: { [key: string]: any } = {
+  multiselectData: { children: IMultiSelect[] } = {
     children: [
       {
-        key: 'Thailand', value: 'thailand', children: [
+        key: 'Thailand',
+        value: 'thailand',
+        children: [
           {
-            key: 'Bangkok', value: 'bangkok', children: [
-              { key: 'Phra Nakhon District', value: 'phra_nakhon_district' }
-            ]
-          }
-        ]
+            key: 'Bangkok',
+            value: 'bangkok',
+            children: [
+              { key: 'Phra Nakhon District', value: 'phra_nakhon_district' },
+            ],
+          },
+        ],
       },
       {
-        key: 'Malaysia', value: 'malaysia', children: [
+        key: 'Malaysia',
+        value: 'malaysia',
+        children: [
           {
-            key: 'Selangor', value: 'selangor', children: [
+            key: 'Selangor',
+            value: 'selangor',
+            children: [
               { key: 'Subang Jaya', value: 'subang_jaya' },
               { key: 'Petaling Jaya', value: 'petaling_jaya' },
               { key: 'Cyberjaya', value: 'cyberjaya' },
-            ]
+            ],
           },
           {
-            key: 'Melaka', value: 'melaka', children: [
+            key: 'Melaka',
+            value: 'melaka',
+            children: [
               { key: 'Melaka Tengah', value: 'melaka_tengah' },
               { key: 'Aloh Gajah', value: 'aloh_gajah' },
-            ]
+            ],
           },
           {
-            key: 'Perak', value: 'perak', children: [
+            key: 'Perak',
+            value: 'perak',
+            children: [
               { key: 'Ipoh', value: 'ipoh' },
               { key: 'Kampar', value: 'kampar' },
-            ]
+            ],
           },
-        ]
+        ],
       },
       {
-        key: 'Singapore', value: 'singapore', children: [
+        key: 'Singapore',
+        value: 'singapore',
+        children: [
           {
-            key: 'Singapore', value: 'singapore', children: [
-              { key: 'Singapore', value: 'singapore' }
-            ]
-          }
-        ]
+            key: 'Singapore',
+            value: 'singapore',
+            children: [{ key: 'Singapore', value: 'singapore' }],
+          },
+        ],
       },
-    ]
+    ],
   };
   multiselectSubmitButtonTemplate: IFieldConfig<IFieldConfigForButtonConfig> = {
     name: 'submit_button',
@@ -62,65 +81,109 @@ export class MultiSelectComponent implements OnInit {
     type: EFieldConfigType.Button,
     type_config: {
       type: 'submit',
-      css_class: { group: 'd-grid gap-2 d-md-flex justify-content-md-end mt-3', button: 'btn btn-primary' }
+      css_class: {
+        group: 'd-grid gap-2 d-md-flex justify-content-md-end mt-3',
+        button: 'btn btn-primary',
+      },
     },
-    css_class: 'col-12'
+    css_class: 'col-12',
   };
 
-  preTemplate: any[] = [
+  preTemplate: IFieldConfig<IFieldConfigForInputConfig | IFieldConfigForSelectConfig>[] = [
     {
       name: 'full_name',
       display_text: 'Full name: ',
       type: EFieldConfigType.Input,
       type_config: {
-        type: 'text',
+        type: EFieldConfigInputType.Text,
         list: false,
-        css_class: { group: 'form-group mb-3', group_label: '', input: 'form-control', input_label: 'mb-1' },
+        css_class: {
+          group: 'form-group mb-3',
+          group_label: '',
+          input: 'form-control',
+          input_label: 'mb-1',
+        },
       },
-      css_class: 'col-12'
+      css_class: 'col-12',
     },
     {
       name: 'country_name',
       display_text: 'Country name: ',
       type: EFieldConfigType.Input,
       type_config: {
-        type: 'text',
+        type: EFieldConfigInputType.Text,
         list: false,
-        css_class: { group: 'form-group mb-3', group_label: '', input: 'form-control', input_label: 'mb-1' },
+        css_class: {
+          group: 'form-group mb-3',
+          group_label: '',
+          input: 'form-control',
+          input_label: 'mb-1',
+        },
       },
-      css_class: 'col-12'
+      css_class: 'col-12',
+    },
+    {
+      name: 'planet',
+      type: EFieldConfigType.Select,
+      type_config: {
+        controls: [
+          {
+            name: 'planet',
+            label: 'Planet',
+            key_field: 'key',
+            value_field: 'value',
+            value: '',
+          },
+        ],
+        css_class: {
+          group: 'col-12 mb-3',
+          select: 'form-select',
+          select_label: 'planet',
+        },
+        dataset: [{ key: 'Earth', value: 'earth' }],
+      },
+      css_class: 'col-md-6'
     },
   ];
 
-  postTemplate: any[] = [
+  postTemplate: IFieldConfig<IFieldConfigForInputConfig>[] = [
     {
       name: 'purpose',
       display_text: 'Purpose: ',
       type: EFieldConfigType.Input,
       type_config: {
-        type: 'text',
+        type: EFieldConfigInputType.Text,
         list: false,
-        css_class: { group: 'form-group mb-3', group_label: '', input: 'form-control', input_label: 'mb-1' },
+        css_class: {
+          group: 'form-group mb-3',
+          group_label: '',
+          input: 'form-control',
+          input_label: 'mb-1',
+        },
       },
-      css_class: 'col-12'
+      css_class: 'col-12',
     },
     {
       name: 'remark',
       display_text: 'Remark: ',
       type: EFieldConfigType.Input,
       type_config: {
-        type: 'text',
+        type: EFieldConfigInputType.Text,
         list: false,
-        css_class: { group: 'form-group mb-3', group_label: '', input: 'form-control', input_label: 'mb-1' },
+        css_class: {
+          group: 'form-group mb-3',
+          group_label: '',
+          input: 'form-control',
+          input_label: 'mb-1',
+        },
       },
-      css_class: 'col-12'
+      css_class: 'col-12',
     },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   formOnSubmitMultiselect(val: any): void {
     console.log(val);
